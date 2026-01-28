6f61cb2c193d779b47015efdba109ad97d8ce35e
Shaady Khatab <shaady.khatab@cariad.technology>
2026-01-28 08:52:13 +0100

    Implementing Gradient Descent Algorithm in Python for course 2 - Intro to Deep Learning

---
+++ C2_Intro2DL/data_prep.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+
+admissions = pd.read_csv('binary.csv')
+
+# Make dummy variables for rank
+data = pd.get_dummies(admissions, prefix='rank', columns=['rank'])
+
+# Standardize features
+for field in ['gre', 'gpa']:
+    mean, std = data[field].mean(), data[field].std()
+    data.loc[:,field] = (data[field]-mean)/std
+    
+# Split off random 10% of the data for testing
+np.random.seed(42)
+sample = np.random.choice(data.index, size=int(len(data)*0.9), replace=False)
+data, test_data = data.loc[sample], data.drop(sample)
+
+# Split into features and targets
+features, targets = data.drop('admit', axis=1), data['admit']
+features_test, targets_test = test_data.drop('admit', axis=1), test_data['admit']
+
+
+      
